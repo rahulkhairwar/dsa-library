9410b2b684a1cd5ba736019b38cc1b312050d191
Rahul Khairwar <rahulkhairwar@gmail.com>
2020-01-20 18:27:58 +0800

    Added new files

---
+++ src/com/codeforces/competitions/educational/year2020/round80/TaskA.java
@@ -0,0 +1,178 @@
+package com.codeforces.competitions.educational.year2020.round80;
+
+import java.io.*;
+import java.util.*;
+
+public class TaskA
+{
+	public static void main(String[] args)
+	{
+		new TaskA(System.in, System.out);
+	}
+
+	static class Solver implements Runnable
+	{
+		int n, d;
+		InputReader in;
+		PrintWriter out;
+
+		void solve() {
+			int t = in.nextInt();
+
+			while (t-- > 0) {
+				n = in.nextInt();
+				d = in.nextInt();
+
+				if (n >= d) {
+					out.println("YES");
+
+					continue;
+				}
+
+				int sqrt = 1 + (int) Math.sqrt(d);
+				int prev = Integer.MAX_VALUE;
+
+				sqrt *= 10;
+
+				for (int x = 1; x <= sqrt; x++) {
+					int val = x + (int) Math.ceil(d / (double) (x + 1));
+
+					if (prev < val) {
+						if (prev <= n) {
+							out.println("YES");
+						} else {
+							out.println("NO");
+						}
+
+						break;
+					}
+
+					prev = val;
+				}
+			}
+		}
+
+		Solver(InputReader in, PrintWriter out)
+		{
+			this.in = in;
+			this.out = out;
+		}
+
+		@Override
+		public void run()
+		{
+			solve();
+		}
+
+	}
+
+	static class InputReader
+	{
+		private InputStream stream;
+		private byte[] buf = new byte[1024];
+		private int curChar;
+		private int numChars;
+
+		public int read()
+		{
+			if (numChars == -1)
+				throw new InputMismatchException();
+
+			if (curChar >= numChars)
+			{
+				curChar = 0;
+				try
+				{
+					numChars = stream.read(buf);
+				}
+				catch (IOException e)
+				{
+					throw new InputMismatchException();
+				}
+				if (numChars <= 0)
+					return -1;
+			}
+
+			return buf[curChar++];
+		}
+
+		public int nextInt()
+		{
+			int c = read();
+
+			while (isSpaceChar(c))
+				c = read();
+
+			int sgn = 1;
+
+			if (c == '-')
+			{
+				sgn = -1;
+				c = read();
+			}
+
+			int res = 0;
+
+			do
+			{
+				if (c < '0' || c > '9')
+					throw new InputMismatchException();
+
+				res *= 10;
+				res += c & 15;
+
+				c = read();
+			} while (!isSpaceChar(c));
+
+			return res * sgn;
+		}
+
+		public boolean isSpaceChar(int c)
+		{
+			return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+		}
+
+		public void close()
+		{
+			try
+			{
+				stream.close();
+			}
+			catch (IOException e)
+			{
+				e.printStackTrace();
+			}
+		}
+
+		InputReader(InputStream stream)
+		{
+			this.stream = stream;
+		}
+
+	}
+
+	public TaskA(InputStream inputStream, OutputStream outputStream)
+	{
+		InputReader in = new InputReader(inputStream);
+		PrintWriter out = new PrintWriter(outputStream);
+		Thread thread = new Thread(null, new Solver(in, out), "TaskA", 1 << 29);
+
+		try
+		{
+			thread.start();
+			thread.join();
+		}
+		catch (InterruptedException e)
+		{
+			e.printStackTrace();
+		}
+		finally
+		{
+			in.close();
+			out.flush();
+			out.close();
+		}
+	}
+
+}
+
